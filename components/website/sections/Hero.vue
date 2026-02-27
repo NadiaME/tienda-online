96137a8ee48be573d2a1c9bb4b5207fc387ed69b
NadiaME <nadiam2328@hotmail.com>
2026-02-27 13:47:11 -0300

    feat: inicio sistema websites dinámicos

---
+++ components/website/sections/Hero.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+defineProps<{ content: any }>()
+</script>
+
+<template>
+  <section>
+    <h1>{{ content.title }}</h1>
+    <p>{{ content.subtitle }}</p>
+  </section>
+</template>
